1ed32d3584c3e9f179ba47a36efbb42d55d07f66
Edward A Maxedon Sr <edadma@gmail.com>
2025-03-04 15:36:32 -0500

    event styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,102 @@
 import { SetStateAction, useState } from 'react'
 import Calendar from './Calendar'
-import { Button, Card } from 'antd'
+import { Badge, Button, Card } from 'antd'
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
 
-  // Sample data for demonstration with multiple events on the same days
+  // Updated sample events for fleet management with the format:
+  // ${h:mma}\u2002${customer.firstName} ${customer.lastName}\u2002{workflow.name}\u2002{store.name}
   const sampleEvents = [
     {
       date: new Date(2025, 2, 3, 9, 30),
       title: '9:30am\u2002John Smith\u2002Shuttle Drop-off\u2002Alpha Store',
-      color: '#4285F4',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 3, 10, 45),
       title: '10:45am\u2002Emily Johnson\u2002Shuttle Pickup\u2002Alpha Store',
-      color: '#0F9D58',
+      color: '#60be23',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 3, 13, 15),
       title: '1:15pm\u2002Michael Brown\u2002Part Delivery\u2002Alpha Store',
-      color: '#DB4437',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 3, 15, 0),
       title: '3pm\u2002Sarah Davis\u2002Part Pickup\u2002Alpha Store',
-      color: '#F4B400',
+      color: '#60be23',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 5, 8, 15),
       title: '8:15am\u2002Robert Wilson\u2002Valet Pickup\u2002Alpha Store',
-      color: '#4285F4',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 8, 11, 30),
       title: '11:30am\u2002Jessica Taylor\u2002Valet Drop-off\u2002Alpha Store',
-      color: '#0F9D58',
-      cancelled: false,
+      color: '#e25263',
+      cancelled: true,
     },
     {
       date: new Date(2025, 2, 10, 14, 45),
       title: '2:45pm\u2002David Martinez\u2002Task\u2002Alpha Store',
-      color: '#DB4437',
+      color: '#60be23',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 10, 16, 30),
       title: '4:30pm\u2002Jennifer Anderson\u2002Shuttle Pickup\u2002Alpha Store',
-      color: '#F4B400',
+      color: '#e25263',
       cancelled: true,
     },
     {
       date: new Date(2025, 2, 15, 9, 0),
       title: '9am\u2002Thomas Robinson\u2002Part Delivery\u2002Alpha Store',
-      color: '#4285F4',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 17, 13, 0),
       title: '1pm\u2002Lisa White\u2002Shuttle Drop-off\u2002Alpha Store',
-      color: '#0F9D58',
+      color: '#60be23',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 20, 10, 15),
       title: '10:15am\u2002James Clark\u2002Valet Pickup\u2002Alpha Store',
-      color: '#DB4437',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 22, 15, 45),
       title: '3:45pm\u2002Patricia Lewis\u2002Task\u2002Alpha Store',
-      color: '#F4B400',
+      color: '#60be23',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 25, 11, 0),
       title: '11am\u2002Christopher Lee\u2002Part Pickup\u2002Alpha Store',
-      color: '#4285F4',
+      color: '#bfbfbf',
       cancelled: false,
     },
     {
       date: new Date(2025, 2, 27, 14, 30),
       title: '2:30pm\u2002Elizabeth Walker\u2002Valet Drop-off\u2002Alpha Store',
-      color: '#0F9D58',
+      color: '#e25263',
       cancelled: true,
     },
     {
       date: new Date(2025, 2, 29, 9, 45),
       title: '9:45am\u2002Daniel Hall\u2002Shuttle Pickup\u2002Alpha Store',
-      color: '#DB4437',
+      color: '#60be23',
       cancelled: false,
     },
   ]
